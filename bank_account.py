f86e643dc31f3ff51726794f2333bf7b80b614a8
LukaszP95 <house456@o2.pl>
2025-01-20 01:18:55 +0100

    Update bank_account.py

--- bank_account.py
+++ bank_account.py
@@ -1,24 +1,3 @@
-"""Klasa Konto Bankowe
-
-Cel:
-Zaprojektuj klasę BankAccount, która będzie symulować podstawowe operacje związane z kontem bankowym.
- Zadanie ma na celu praktyczne zastosowanie programowania obiektowego, w tym pracy z atrybutami, metodami oraz prostą logiką biznesową.
-
-Pola:
-- owner: str - imie i nazwisko wlasciciela
-- balance: float, domyślnie ma wartość 0 - stan konta
-
-Metody:
-- get_balance() - zwraca Ci aktualny stan konta
-- deposit() - wpłaca pieniądze na konto
-- withdraw() - wypłaca pieniądze z konta
-
-Standardowo po if _name_ ... tworzysz instancje konta, i wywołujesz kilka razy różne metody, żeby sprawdzić czy dziala poprawnie
-to dodasz walidacje:
-- wprowadzona kwota nie może być ujemna, np. nie możesz wpłacić -200 zł
-- jeśli na koncie jest 100 zł, to nie możesz wypłacić 150zł - powinien zostać wyrzucony wyjątek"""
-
-
 class BankAccount:
 
 
@@ -59,4 +38,3 @@ if __name__ == "__main__":
     account.withdraw(200)
     print("Balance:" , account.get_balance())
     print(f"account owner: {account.owner} \naccount balance: {account.balance}")
-
